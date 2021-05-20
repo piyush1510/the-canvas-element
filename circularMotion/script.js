4390bbc593de8c54c93e038d220d68a8d5105195
Piyush Verma <piyus15101999@gmail.com>
2021-05-21 01:43:10 +0530

    circling effect created

--- circularMotion/script.js
+++ circularMotion/script.js
@@ -1,47 +1,68 @@
-var canvas = /** @type {HTMLCanvasElement} */ (document.querySelector('#canvas'))
+var canvas = /** @type {HTMLCanvasElement} */ (
+  document.querySelector("#canvas")
+);
 
-var c=canvas.getContext('2d')
-const w=window.innerWidth;
-const h=window.innerHeight;
-canvas.width=w;
-canvas.height=h
-const colors=['#e56a6b',"#ffd6de","#ffccb0","#49616d"]
+var c = canvas.getContext("2d");
+const w = window.innerWidth;
+const h = window.innerHeight;
+canvas.width = w;
+canvas.height = h;
+const colors = ["#e56a6b", "#ffd6de", "#ffccb0", "#49616d"];
 
-
-var mouse={
-    x:0,
-    y:0
+var mouse = {
+  x: w / 2,
+  y: h / 2,
+};
+addEventListener("mousemove", (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+addEventListener("mouseout", (event) => {
+  mouse.x = w / 2;
+  mouse.y = h / 2;
+});
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-addEventListener('mousemove',event=>{
-    mouse.x=event.clientX;
-    mouse.y=event.clientY
-})
-
-function randomInt(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min)
-}
-function randomColor(){
-    return colors[randomInt(0,colors.length-1)]
-}
-
-class object{
-    constructor(){
-        this.x=100;
-        this.y=100;
-        this.color=randomColor();
-        this.radius=20;
-    }
-    draw(){
-        c.beginPath();
-        c.arc(this.x,this.y,0,Math.PI*2,false)
-        c.fillStyle=this.color;
-        c.closePath();
-    }
+function randomColor() {
+  return colors[randomInt(0, colors.length - 1)];
 }
 
-function animate(){
-    requestAnimationFrame(animate);
-    c.clearRect(0,0,w,h);
-    c.fillText("html canvas",mouse.x,mouse.y)
+class Particle {
+  constructor() {
+    this.y = h / 2;
+    this.color = randomColor();
+    this.radius = 5;
+    this.x = w / 2;
+    this.radians = Math.random() * Math.PI * 2;
+    this.velocity = 0.01;
+    this.ds = 100 * Math.random() + 100;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
+  update() {
+    this.x = mouse.x + Math.cos(this.radians) * this.ds;
+    this.y = mouse.y + Math.sin(this.radians) * this.ds;
+    this.radians += this.velocity;
+  }
+}
+var particles = [];
+for (let i = 0; i < 200; i++) {
+  particles.push(new Particle());
+}
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle='rgba(255,255,255,0.05)'
+  c.fillRect(0, 0, w, h);
+  particles.forEach((particle) => {
+    particle.update();
+    particle.draw();
+  });
 }
 animate();
+// this one was from a tutorial
